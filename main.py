1da429c190862e3f1a1171cb68c09f6b15d533df
gongcheng <1415386588@qq.com>
2025-08-04 15:31:09 +0800

    提交

--- main.py
+++ main.py
@@ -2,6 +2,25 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import torch, torchvision
+import cv2 as cv
+from torch import nn, optim
+from torchvision import transforms
+import matplotlib.pyplot as plt; import seaborn as sns
+from matplotlib import cm
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5 import QtGui
+import onnx; import onnxruntime as ort
+from onnx import helper, checker
+import numpy as np; from numpy import linspace as lsp
+from pandas import DataFrame as DF, read_csv as rc
+import scipy.stats
+from scipy import signal
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from keras.models import Sequential
+from keras import optimizers
 
 
 def print_hi(name):
